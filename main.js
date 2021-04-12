df0b135574e817eb1baac3ce6026ab057f732f20
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-04-12 13:31:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-var student = [
-  {name : 'ram' , age : 18},
-  {name : 'jon' , age : 20},
-  {name : 'pit' , age : 21},
-];
+var element;
 
+element = document.getElementById("id")
 
-console.log(student);
-
-for(var a = 0; a<student.length ;a++){
-  document.write(student[a].name + "<br");
-}
+console.log(element);
