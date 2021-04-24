fe331a780555941fc3dd7f04b4b98e9a86bc0e04
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-04-24 22:28:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-var element;
+console.log('this is tute 16');
 
-element = document.getElementsByClassName("id")
+let element = document.createElement('li');
+
+element.className = 'new-list';
+element.id = 'new-id';
+element.innerText = 'I am web dev'
+
+let ul = document.querySelector('ul.this');
+ul.appendChild(element)
+console.log(ul);
 
 console.log(element);
+
+
+let elem2 = document.createElement('h3');
+elem2.id = 'heading';
+elem2.className = 'heading';
+elem2.innerText = 'haii every buddy is present' ;
+
+let cont = document.createTextNode('I will be go to my roomś');
+
+elem2.appendChild(cont)
+
+
+let rep = document.getElementById('this');
+
+rep.replaceChild(element, document.getElementById('myid'));
+
+console.log(rep );
